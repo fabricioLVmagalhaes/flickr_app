54f7d6369b2b3ddcfae0ef496d3d39197f9d2f2a
Fabricio Magalhães <fabricio.lvm@gmail.com>
2019-11-27 22:10:05 -0300

    Search and PhotoDetail Activities

---
+++ app/src/main/java/com/magalhaes/flickrapp/SearchAcitivity.kt
@@ -0,0 +1,20 @@
+package com.magalhaes.flickrapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_search.*
+
+class SearchAcitivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        setSupportActionBar(toolbar)
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
