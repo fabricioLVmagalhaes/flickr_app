f9f1166e996d7d1448a187a06ab973667859fe87
Fabricio Magalhães <fabricio.lvm@gmail.com>
2019-12-06 23:00:08 -0300

    Gesture Detector

--- app/src/main/java/com/magalhaes/flickrapp/RecyclerItemClickListener.kt
+++ app/src/main/java/com/magalhaes/flickrapp/RecyclerItemClickListener.kt
@@ -2,8 +2,10 @@ package com.magalhaes.flickrapp
 
 import android.content.Context
 import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerItemClickListener(context: Context, recyclerView: RecyclerView, private val listener: OnRecyclerClickListener)
@@ -15,9 +17,28 @@ class RecyclerItemClickListener(context: Context, recyclerView: RecyclerView, pr
         fun onItemLongClick(view: View, position: Int)
     }
 
+    private val gestureDetector = GestureDetectorCompat(context, object : GestureDetector.SimpleOnGestureListener(){
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Log.d(TAG, ".onSingleTupUp: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            Log.d(TAG, ".onSingleTupUp calling listener .onItemClick")
+            listener.onItemClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            Log.d(TAG, ".onLongPress: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            Log.d(TAG, ".onSingleTupUp calling listener .onItemLongClick")
+            listener.onItemLongClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+        }
+    })
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
         Log.d(TAG, ".onInterceptTouchEvent: starts $e")
         //return super.onInterceptTouchEvent(rv, e)
-        return true
+        val result = gestureDetector.onTouchEvent(e)
+        Log.d(TAG, ".onInterceptTouchEvent() returning: $result")
+        return result
     }
 }
