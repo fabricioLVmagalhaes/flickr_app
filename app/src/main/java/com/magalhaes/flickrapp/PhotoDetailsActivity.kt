676808d02966a447d9dad647f6bd54edfc4d17a0
Fabricio Magalhães <fabricio.lvm@gmail.com>
2019-12-07 23:34:44 -0300

    Modify Photo Details Activity

--- app/src/main/java/com/magalhaes/flickrapp/PhotoDetailsActivity.kt
+++ app/src/main/java/com/magalhaes/flickrapp/PhotoDetailsActivity.kt
@@ -1,17 +1,27 @@
 package com.magalhaes.flickrapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_photo_details.*
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
 class PhotoDetailsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
+
         activateToolbar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+
+        Picasso.with(this).load(photo.link)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(photo_image)
     }
 
 }
