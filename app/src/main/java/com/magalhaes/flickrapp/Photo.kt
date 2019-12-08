1dc0f5e8980010177d07c13a6cd7011ef02a932d
Fabricio Magalhães <fabricio.lvm@gmail.com>
2019-12-07 23:25:10 -0300

    Serializable

--- app/src/main/java/com/magalhaes/flickrapp/Photo.kt
+++ app/src/main/java/com/magalhaes/flickrapp/Photo.kt
@@ -1,8 +1,52 @@
 package com.magalhaes.flickrapp
 
-class Photo(val title: String, val author: String, val authorId: String, val link: String, val tags: String, val image: String) {
+import android.util.Log
+import java.io.IOException
+import java.io.ObjectInputStream
+import java.io.ObjectStreamException
+import java.io.Serializable
+
+class Photo(
+    var title: String,
+    var author: String,
+    var authorId: String,
+    var link: String,
+    var tags: String,
+    var image: String
+) : Serializable {
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
 
     override fun toString(): String {
         return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tags='$tags', image='$image')"
+    }
+
+    @Throws(IOException::class)
+    private fun writeObject(out: java.io.ObjectOutputStream) {
+        Log.d("Photo", "wirteObject called")
+        out.writeUTF(title)
+        out.writeUTF(author)
+        out.writeUTF(authorId)
+        out.writeUTF(link)
+        out.writeUTF(tags)
+        out.writeUTF(image)
+    }
+
+    @Throws(IOException::class, ClassNotFoundException::class)
+    private fun readObject(inputStream: ObjectInputStream) {
+        Log.d("Photo", "readObject called")
+        title = inputStream.readUTF()
+        author = inputStream.readUTF()
+        authorId = inputStream.readUTF()
+        link = inputStream.readUTF()
+        tags = inputStream.readUTF()
+        image = inputStream.readUTF()
+    }
+
+    @Throws(ObjectStreamException::class)
+    private fun readObjectNoData() {
+        Log.d("Photo", "readObjectNoData called")
     }
 }
